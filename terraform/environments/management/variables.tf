cfba673ba6eabf3cc4565d4bd84c181f8ebf712f
Martin Fox <m.fox2@kainos.com>
2018-09-04 14:03:37 +0100

    Quick fix for 2 syntax errors

--- terraform/environments/management/variables.tf
+++ terraform/environments/management/variables.tf
@@ -1,7 +1,7 @@
 #Variables for the Management Environment
 
 variable "environment" {
-    default = "mgmt"
+  default = "mgmt"
 }
 
 variable "security_group_name" {
@@ -9,23 +9,23 @@ variable "security_group_name" {
 }
 
 variable "dmz_cidr" {
-    default = "10.120.0.0/24"
+  default = "10.120.0.0/24"
 }
 
 variable "preview_cidr" {
   default = "10.122.0.0/16"
 }
-=======
+
 #EC2 instance variables
 
 variable "ami" {
-    default = "ami-00846a67"
+  default = "ami-00846a67"
 }
 
 variable "jenkins_instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "jenkins_name" {
-    default = "jenkins"
+  default = "jenkins"
 }
--- terraform/environments/preview/variables.tf
+++ terraform/environments/preview/variables.tf
@@ -1,42 +1,39 @@
 #Variables for Preview Environment
 
 variable "environment" {
-    default = "preview"
+  default = "preview"
 }
 
-
 variable "preview_wordpress_cidr" {
-    default = "10.122.0.0/24"
+  default = "10.122.0.0/24"
+}
 
 variable "dmz_subnet" {
-    default = "10.120.0.0/24"
+  default = "10.120.0.0/24"
 }
 
 #DB Server
 variable "db_security_group" {
-    default = "db_rds_sg"
+  default = "db_rds_sg"
 }
-
 
 #Proxy Servers
 
 variable "proxy_subnet" {
-    default = "10.122.0.0/16"
+  default = "10.122.0.0/16"
 }
-
 
 #Wordpress Servers
 
 variable "wp_servers_security_group" {
-    default = "wp_servers"
-
+  default = "wp_servers"
 }
 
 #Preview Cidr
 variable "preview_webserver_cidr" {
-    default = "10.120.1.0/24"
+  default = "10.120.1.0/24"
 }
 
 variable "preview_db_cidr" {
-    default = "10.120.3.0/24"
+  default = "10.120.3.0/24"
 }

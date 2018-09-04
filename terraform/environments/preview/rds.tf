34c01fe0a75b2dad0fca39968d10a8648f6c6470
Jack King <jackk@crete.local>
2018-09-04 16:23:17 +0100

    Updated RDS Preview/README

--- terraform/environments/preview/rds.tf
+++ terraform/environments/preview/rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "preview_rds" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "${var.preview_rds}"
-  username             = "wpusers"
+  username             = "wpuser"
   password             = "12345"
   parameter_group_name = "default.mysql5.7"
 }

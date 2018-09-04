54bf70c17bc3232ff8ddd3fb44df20a356601ed2
Marc Hughes <M.Hughes@kainos.com>
2018-09-04 10:05:21 +0100

    Adding Provider

--- terraform/environments/preview/main.tf
+++ terraform/environments/preview/main.tf
@@ -1,8 +1,29 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-047bb4163c506cd98"
-  instance_type = "t2.micro"
+resource "aws_route53_zone" "main" {
+  name = "woabelfast.co.uk"
+}
+
+resource "aws_route53_zone" "preview" {
+  name = "preview.woabelfast.co.uk"
+
+  tags {
+    Environment = "preview"
+  }
+}
+
+resource "aws_route53_record" "preview-ns" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "woabelfast.co.uk"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.preview.name_servers.0}",
+    "${aws_route53_zone.preview.name_servers.1}",
+    "${aws_route53_zone.preview.name_servers.2}",
+    "${aws_route53_zone.preview.name_servers.3}",
+  ]
 }

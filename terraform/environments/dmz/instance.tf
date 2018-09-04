8d2fb086b5118a8cb3b9bc7b842116f3d9e6f71d
Cameron Sharp <camerons@Phoenicia.local>
2018-09-04 16:44:36 +0100

    altering the instance file to use keypair

--- terraform/environments/dmz/instance.tf
+++ terraform/environments/dmz/instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "jumpbox" {
     instance_type = "${var.instance_type}"
     subnet_id = "${aws_subnet.dmz_subnet.id}"
     security_groups = ["${aws_security_group.dmz_sg.id}"]
+    key_name = "${var.jumpbox_key}"
 
    tags {
        Name = "${var.instance_name}"

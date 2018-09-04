265c32f215e56c9a3245bf6103deb8c63b808d54
Jack King <jackk@crete.local>
2018-09-04 16:10:36 +0100

    Created RDS Preview Instance

---
+++ terraform/environments/preview/rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "preview_rds" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "${var.preview_rds}"
+  username             = "wpusers"
+  password             = "12345"
+  parameter_group_name = "default.mysql5.7"
+}
--- terraform/environments/preview/variables.tf
+++ terraform/environments/preview/variables.tf
@@ -37,3 +37,8 @@ variable "preview_webserver_cidr" {
 variable "preview_db_cidr" {
   default = "10.120.3.0/24"
 }
+
+#Preview RDS
+variable "preview_rds" {
+  default = "wordpress"
+}

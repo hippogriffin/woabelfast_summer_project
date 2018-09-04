752489888d20f21bc86d7999e7a0d0acf601b6fb
Marc Hughes <M.Hughes@kainos.com>
2018-09-04 11:08:13 +0100

    typo in vpc_id

--- terraform/environments/dmz/main.tf
+++ terraform/environments/dmz/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_route53_zone" "private_dns" {
   name = "woabelfast.local"
-  vpc_id  = "${aws_vpc.dmz.id}"
+  vpc_id  = "${aws_vpc.dmz_vpc.id}"
   tags {
     Name = "private_zone"
   }

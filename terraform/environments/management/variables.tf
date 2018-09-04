445475b118bd4c44bbeb0661428e3ee1b906b7cb
Enda <e.kelly@kainos.com>
2018-09-04 10:44:08 +0100

    previous commit did not work

--- terraform/environments/management/variables.tf
+++ terraform/environments/management/variables.tf
@@ -1,19 +1,4 @@
 variable "woabelfast_security-group" {
-  default = "woabelfast_security-group"
-}
-
-variable "woabelfast_dmz-subnet" {
-  default = "10.120.0.0/24"
-}
-variable "woabelfast_mgmt-subnet" {
-  default = "10.121.0.0/24"
-}
-
-variable "woabelfast_environment-name" {
-  default = "management"
-}
-
-variable "woabelfast_vpc" {
-  default = ""
+  default = "mgmt_sg"
 }
 

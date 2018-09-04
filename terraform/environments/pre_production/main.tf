ed0c359142387f295ced77e3d79b4ee5a338e32e
Marc Hughes <M.Hughes@kainos.com>
2018-09-04 10:17:31 +0100

    adding across al environments

--- terraform/environments/pre_production/main.tf
+++ terraform/environments/pre_production/main.tf
@@ -2,3 +2,28 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_route53_zone" "main" {
+  name = "woabelfast.co.uk"
+}
+
+resource "aws_route53_zone" "pre-production" {
+  name = "pre_production.woabelfast.co.uk"
+
+  tags {
+    Environment = "pre_production"
+  }
+}
+
+resource "aws_route53_record" "pre-production-ns" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name    = "woabelfast.co.uk"
+  type    = "NS"
+  ttl     = "30"
+
+  records = [
+    "${aws_route53_zone.pre-production.name_servers.0}",
+    "${aws_route53_zone.pre-production.name_servers.1}",
+    "${aws_route53_zone.pre-production.name_servers.2}",
+    "${aws_route53_zone.pre-production.name_servers.3}",
+  ]
+}

80db00fba656fcc36d107a420630cd6f3cf532d5
jamesni1 <j.nicholl@kainos.com>
2018-09-04 12:39:43 +0100

    Update instance.tf

--- terraform/environments/management/instance.tf
+++ terraform/environments/management/instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Jenkins" {
     ami = "${var.ami}"
     instance_type = "${var.jenkins_instance_type}"
     subnet_id = "${aws_subnet.management_subnet.id}"
-    security_groups = ["${aws_security_group.management_sg.name}"]
+    security_groups = ["${aws_security_group.management_sg.id}"]
 
     tags {
         Name = "${var.jenkins_name}"

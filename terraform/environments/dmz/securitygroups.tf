cf76fb3262b57adce3fc6ce6d9b9454761e9c6c5
James Nicholl <jamesni@kainos.com>
2018-09-04 11:02:51 +0100

    removed egress rules and variables for preview/preprod/prod

--- terraform/environments/dmz/securitygroups.tf
+++ terraform/environments/dmz/securitygroups.tf
@@ -1,6 +1,6 @@
 #Configuring DMZ Security Group
 resource "aws_security_group" "dmz_sg" {
-    name = "dmz_sg"
+    name = "${var.security_group_name}"
     description = "Allows ssh, http and https to bastion servers"
     vpc_id = "${aws_vpc.dmz_vpc.id}"
 
@@ -25,22 +25,8 @@ resource "aws_security_group" "dmz_sg" {
         cidr_blocks = ["${var.kainos_cidr}"]
     }
 
-    egress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["${var.mgmt_cidr}", "${var.preview_cidr}", "${var.preprod_cidr}", "${var.production_cidr}"]
-    }
-
-    egress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["${var.mgmt_cidr}"]
-    }
-
     tags {
-        name = "DMZ_SG"
+        name = "${var.security_group_name}"
         terraform = "true"
     }
 }
--- terraform/environments/dmz/variables.tf
+++ terraform/environments/dmz/variables.tf
@@ -10,18 +10,6 @@ variable "mgmt_cidr" {
     default = "10.121.0.0/24"
 }
 
-variable "preview_cidr" {
-    default = "10.122.0.0/24"
-}
-
-variable "preprod_cidr" {
-    default = "10.123.0.0/24"
-}
-
-variable "production_cidr" {
-    default = "10.124.0.0/24"
-}
-
 variable "security_group_name" {
     default = "dmz_sg"
 }

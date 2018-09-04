1be784ae88ae868b0688d22fedf6d59572f07e67
Enda <e.kelly@kainos.com>
2018-09-04 13:31:51 +0100

    updated managment/securitygroups.tf

--- terraform/environments/management/main.tf
+++ terraform/environments/management/main.tf
@@ -1,26 +1,3 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-#configure management security group
-resource "aws_security_group" "${var.woabelfast_security-group}" {
-    vpc_id = "${aws_vpc.woabelfast_vpc.id}"
-
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = "${aws_subnet.dmz_subnet.id}"
-  }
-
-  egress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = "${aws_subnet.dmz_subnet.id}"
-
-tags {
-    Name = "Allow SSH from ${var.woabelfast_mgmt-subnet} to/from ${var.woabelfast_dmz-subnet}"
-}
-}
-}

16e010a9ba3033ceba5c7e0e18ef0118da10560d
Marc Hughes <M.Hughes@kainos.com>
2018-09-04 16:07:22 +0100

    Adding updated instance was missing subnets

--- terraform/environments/preview/instance.tf
+++ terraform/environments/preview/instance.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "proxy-servers" {
 ami = "${var.ami}"
 count = 2
 instance_type = "${var.instance_type}"
+subnet_id = "${aws_subnet.preview_webserver_subnet.id}"
+security_groups = ["${aws_security_group.preview_web_servers.id}"]
 private_ip = "${lookup(var.webservers_ips, count.index)}"
 
   tags {

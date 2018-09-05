676a51b27cc6f72ab2d7f2fc9b102032eed73b1c
Enda <e.kelly@kainos.com>
2018-09-05 12:39:38 +0100

    removed internet gw and references from gateways.tf. added route assocication between mgmt_subnet and mgmt_private_route_table

--- terraform/environments/management/gateways.tf
+++ terraform/environments/management/gateways.tf
@@ -1,26 +1,10 @@
-resource "aws_internet_gateway" "mgmt_int_gw" {
-  vpc_id = "${aws_vpc.mgmt_vpc.id}"
-
-  tags {
-    Name = "mgmt_int_gw"
-  }
-}
-
-resource "aws_route" "mgmt_internet_access" {
-  route_table_id         = "${aws_vpc.mgmt_vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.mgmt_int_gw.id}"
-}
-
 resource "aws_eip" "mgmt_nat_gw_eip" {
   vpc      = true
-  depends_on = ["aws_internet_gateway.mgmt_int_gw"]
 }
 
 resource "aws_nat_gateway" "mgmt_nat_gw" {
     allocation_id = "${aws_eip.mgmt_nat_gw_eip.id}"
     subnet_id = "${aws_subnet.mgmt_subnet.id}"
-    depends_on = ["aws_internet_gateway.mgmt_int_gw"]
 }
 
 resource "aws_route_table" "mgmt_private_route_table" {
@@ -33,6 +17,13 @@ resource "aws_route_table" "mgmt_private_route_table" {
 
 resource "aws_route" "mgmt_private_route_table" {
 	route_table_id  = "${aws_route_table.mgmt_private_route_table.id}"
+  
 	destination_cidr_block = "0.0.0.0/0"
 	nat_gateway_id = "${aws_nat_gateway.mgmt_nat_gw.id}"
 }
+
+resource "aws_route_table_association" "mgmt_private_route_table_association" {
+  subnet_id      = "${aws_subnet.mgmt_subnet.id}"
+  route_table_id = "${aws_route_table.mgmt_private_route_table.id}"
+}
+

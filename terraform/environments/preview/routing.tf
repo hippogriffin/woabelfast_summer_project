1a6adba47e4228a8b813eb2a24855d606d912362
jamesni1 <j.nicholl@kainos.com>
2018-09-05 13:45:48 +0100

    Update routing.tf

--- terraform/environments/preview/routing.tf
+++ terraform/environments/preview/routing.tf
@@ -1,6 +1,15 @@
-# Creating a Routing Table of Preview
-resource "aws_route_table" "preview_rt" {
-  vpc_id = "${aws_vpc.preview_vpc.id}"
+resource "aws_route_table" "private_route_table" {
+    vpc_id = "${aws_vpc.preview_vpc.id}"
+
+    tags {
+        Name = "Private route table"
+    }
+}
+
+resource "aws_route" "private_route" {
+	route_table_id  = "${aws_route_table.private_route_table.id}"
+	destination_cidr_block = "0.0.0.0/0"
+	nat_gateway_id = "${aws_nat_gateway.gw.id}"
 }
 
 # Creating a route for vpc_peering between Preview and Management
@@ -8,4 +17,16 @@ resource "aws_route" "preview_to_mgmt" {
   route_table_id            = "${aws_route_table.preview_rt.id}"
   destination_cidr_block    = "${var.mgmt_subnet}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt_to_preview.id}"
+}
+
+# Associate subnet private_1_subnet_eu_west_1a to private route table
+resource "aws_route_table_association" "pr_1_subnet_eu_west_1a_association" {
+    subnet_id = "${aws_subnet.preview_wordpress.id}"
+    route_table_id = "${aws_route_table.private_route_table.id}"
+}
+
+# Associate subnet private_2_subnet_eu_west_1a to private route table
+resource "aws_route_table_association" "pr_2_subnet_eu_west_1a_association" {
+    subnet_id = "${aws_subnet.preview_webserver_subnet.id}"
+    route_table_id = "${aws_route_table.private_route_table.id}"
 }

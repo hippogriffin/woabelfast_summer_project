10b787d1edf72ae9acc48d794ab617d8d22b0b5b
Marty <martin.fox@kainos.com>
2018-09-05 14:29:04 +0100

    Update routing.tf

--- terraform/environments/preview/routing.tf
+++ terraform/environments/preview/routing.tf
@@ -14,7 +14,7 @@ resource "aws_route" "private_route" {
 
 # Creating a route for vpc_peering between Preview and Management
 resource "aws_route" "preview_to_mgmt" {
-  route_table_id            = "${aws_route_table.preview_rt.id}"
+  route_table_id            = "${aws_route_table.private_route_table.id}"
   destination_cidr_block    = "${var.mgmt_subnet}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.mgmt_to_preview.id}"
 }

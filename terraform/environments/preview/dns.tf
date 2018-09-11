0a7c4e65f0dcdebe5e2b1db237c40400354a860b
Jack King <jackk@crete.local>
2018-09-11 11:34:18 +0100

    Changes

--- terraform/environments/preview/dns.tf
+++ terraform/environments/preview/dns.tf
@@ -40,3 +40,13 @@ resource "aws_route53_record" "preview_private_dns_records_db" {
     "${aws_db_instance.preview_rds.address}",
   ]
 }
+
+resource "aws_route53_record" "preview_wordpress_elb" {
+  zone_id = "${aws_route53_zone_association.preview_private_zone.zone_id}"
+  name = "${local.preview_wp_server_elb}"
+  type = "CNAME"
+  ttl = "300"
+   records = [
+    "${aws_elb.preview_wordpress_elb.dns_name}",
+  ]
+} 

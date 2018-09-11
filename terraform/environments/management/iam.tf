80511030c0218f3d3c4fb8db842f2884008adb5c
James Nicholl <jamesni@kainos.com>
2018-09-11 15:30:27 +0100

    fixed iam errors

--- terraform/environments/management/iam.tf
+++ terraform/environments/management/iam.tf
@@ -5,18 +5,14 @@ resource "aws_iam_user" "jenkins" {
     path = "/system/"
 }
 
-resource "aws_iam_access_key" "jenkins" {
-    user = "${aws_iam_user.jenkins.name}"
-}
-
 resource "aws_iam_user_policy" "jenkins" {
     name = "jenkins_policy"
     user = "${aws_iam_user.jenkins.name}"
 
     policy = <<EOF
 {
-    "Version": "2018-09-07",
-    "Statement":[
+    "Version": "2012-10-17",
+    "Statement": [
     {
         "Action": [
             "ec2:*",

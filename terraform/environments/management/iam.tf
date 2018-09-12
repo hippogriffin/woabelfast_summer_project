e560328faa5d57d3f7b030c9e929756cd5728233
Brian McGowan <brianmc@bella.local>
2018-09-12 16:55:59 +0100

    Jenkins IAM roles for permissions - Brian

--- terraform/environments/management/iam.tf
+++ terraform/environments/management/iam.tf
@@ -1,5 +1,53 @@
-#IAM for Jenkins User
+#EC2 IAM role 
+resource "aws_iam_role" "ec2_iam_role" {
+  name = "ec2_iam_role"
 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# EC2 Role Policy
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = "${aws_iam_role.ec2_iam_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# EC2 Instance Profile
+resource "aws_iam_instance_profile" "ec2_iam_profile" {
+  name = "ec2_iam_profile"
+  role = "${aws_iam_role.ec2_iam_role.name}"
+}
+
+
+#IAM for Jenkins User
 resource "aws_iam_user" "jenkins" {
     name = "jenkins"
     path = "/system/"
--- terraform/environments/management/instance.tf
+++ terraform/environments/management/instance.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "Jenkins" {
     subnet_id = "${aws_subnet.mgmt_subnet.id}"
     vpc_security_group_ids = ["${aws_security_group.mgmt_sg.id}"]
     user_data = "${file("scripts/init.cfg")}"
+    iam_instance_profile = "${aws_iam_instance_profile.ec2_iam_profile.name}"
 
     tags {
         Name = "${var.jenkins_name}"

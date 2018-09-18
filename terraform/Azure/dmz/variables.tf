fa0ef453e5d41db4018e48383687d7442febf123
Enda <e.kelly@kainos.com>
2018-09-18 10:05:30 +0100

    removed subnet and cidr variables

--- terraform/Azure/dmz/variables.tf
+++ terraform/Azure/dmz/variables.tf
@@ -6,14 +6,6 @@ variable "dmz_vnet_address_space" {
     default = "172.16.0.0/16"
 }
 
-variable "dmz_subnet_name" {
-    default = "dmz_subnet"
-}
-
-variable "dmz_subnet_cidr" {
-    default = "172.17.0.0/24"
-}
-
 variable "dmz_rg_name" {
     default = "dmz_rg"
 }

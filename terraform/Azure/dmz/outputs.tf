32fe252c5280ee704e10c24d392acc1d06d0e8b3
Brian McGowan <brianmc@bella.local>
2018-09-20 16:03:06 +0100

    Setting up shared storage for wordpress servers

--- terraform/Azure/dmz/outputs.tf
+++ terraform/Azure/dmz/outputs.tf
@@ -1,10 +1,10 @@
 # Create output for preview environment to use for vnet peering
 output "dmz_rg_id" 
 {
-  value = "${azurerm_resource_group.dmz_rg.id}"
+  value = "${basename(azurerm_resource_group.dmz_rg.id)}"
 }
 
 output "dmz_vnet_id" 
 {
-  value = "${azurerm_virtual_network.dmz_vnet.id}"
+  value = "${basename(azurerm_virtual_network.dmz_vnet.id)}"
 }

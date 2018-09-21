6757cfbbd402d6a593a1dfe7fe5b76df23c1acca
Brian McGowan <brianmc@bella.local>
2018-09-21 14:26:20 +0100

    Updating vnet peering

--- terraform/Azure/dmz/outputs.tf
+++ terraform/Azure/dmz/outputs.tf
@@ -1,7 +1,7 @@
 # Create output for preview environment to use for vnet peering
-output "dmz_rg_id" 
+output "dmz_rg_name" 
 {
-  value = "${basename(azurerm_resource_group.dmz_rg.id)}"
+  value = "${azurerm_resource_group.dmz_rg.name}"
 }
 
 output "dmz_vnet_id" 
--- terraform/Azure/preview/vnet_peering.tf
+++ terraform/Azure/preview/vnet_peering.tf
@@ -1,15 +1,20 @@
 # Peering connection - Preview to DMZ
 resource "azurerm_virtual_network_peering" "peering_preview_to_dmz" {
-  name                      = "peer-preview-dmz"
-  resource_group_name       = "${azurerm_resource_group.preview_rg.name}"
-  virtual_network_name      = "${azurerm_virtual_network.preview_vnet.name}"
-  remote_virtual_network_id = "${data.terraform_remote_state.dmz_remote_state.dmz_vnet_id}"
+  name                         = "peer-preview-dmz"
+  resource_group_name          = "${azurerm_resource_group.preview_rg.name}"
+  virtual_network_name         = "${azurerm_virtual_network.preview_vnet.name}"
+  remote_virtual_network_id    = "${data.terraform_remote_state.dmz_remote_state.dmz_vnet_id}"
+  use_remote_gateways          = true
+  allow_forwarded_traffic      = true
+  allow_virtual_network_access = true
 }
 
 # Peering connection - DMZ to Preview
 resource "azurerm_virtual_network_peering" "peering_dmz_to_preview" {
-  name                      = "peer-dmz-preview"
-  resource_group_name       = "${data.terraform_remote_state.dmz_remote_state.dmz_rg_id}"
-  virtual_network_name      = "${data.terraform_remote_state.dmz_remote_state.dmz_vnet_name}"
-  remote_virtual_network_id = "${azurerm_virtual_network.preview_vnet.name}"
+  name                         = "peer-dmz-preview"
+  resource_group_name          = "${data.terraform_remote_state.dmz_remote_state.dmz_rg_name}"
+  virtual_network_name         = "${data.terraform_remote_state.dmz_remote_state.dmz_vnet_name}"
+  remote_virtual_network_id    = "${azurerm_virtual_network.preview_vnet.id}"
+  allow_gateway_transit        = true
+  allow_virtual_network_access = true
 }

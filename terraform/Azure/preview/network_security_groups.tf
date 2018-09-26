1b9911ab897732c53e255c28b02672ca2f19ccae
Marc Hughes <M.Hughes@kainos.com>
2018-09-26 10:25:40 +0100

    Removing all new rules from preview web server

--- terraform/Azure/preview/network_security_groups.tf
+++ terraform/Azure/preview/network_security_groups.tf
@@ -158,18 +158,6 @@ resource "azurerm_network_security_group" "preview_webservers_sg" {
     destination_address_prefix = "*"
   }
 
-    security_rule {
-    name                       = "preview_aws_vpn_sr2"
-    priority                   = 109 // this is the highest so may be subject to change
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "22"
-    destination_port_range     = "22"
-    source_address_prefix      = "52.18.188.26" 
-    destination_address_prefix = "10.120.0.0/24"
-  }
-
   
   tags {
     environment = "${var.environment}"

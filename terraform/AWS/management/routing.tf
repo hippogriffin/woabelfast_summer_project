2d8bcac03940a277bb29ba6e5d0536cf6901326f
Enda <e.kelly@kainos.com>
2018-09-26 12:31:22 +0100

    added route to mgmt_public_route_table to allow traffic to/from dmz

--- terraform/AWS/management/routing.tf
+++ terraform/AWS/management/routing.tf
@@ -25,6 +25,12 @@ resource "aws_route" "mgmt_to_dmz" {
   vpc_peering_connection_id = "${aws_vpc_peering_connection.dmz_mgmt_vpc_peer.id}"
 }
 
+resource "aws_route" "mgmt_to_dmz_public_route" {
+        route_table_id = "${aws_route_table.mgmt_public_route_table.id}"
+        destination_cidr_block = "${var.dmz_cidr}"
+        vpc_peering_connection_id = "${aws_vpc_peering_connection.dmz_mgmt_vpc_peer.id}"
+    }
+
 resource "aws_route_table" "mgmt_public_route_table" {
   vpc_id = "${aws_vpc.mgmt_vpc.id}"
 

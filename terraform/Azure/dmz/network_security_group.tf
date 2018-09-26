b757a8e4c694705de02407350fbe26130616de92
Marc Hughes <M.Hughes@kainos.com>
2018-09-26 10:27:53 +0100

    Updating source port

--- terraform/Azure/dmz/network_security_group.tf
+++ terraform/Azure/dmz/network_security_group.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "dmz" {
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "TCP"
-    source_port_range          = "22"
+    source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*" 
     destination_address_prefix = "Internet"

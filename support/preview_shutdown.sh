b272aca88a828f74d6c2aea55eece9b03dc40633
Brian McGowan <brianmc@bella.local>
2018-10-03 10:39:48 +0100

    Adding parameters for the startup, shutdown scripts

--- support/preview_shutdown.sh
+++ support/preview_shutdown.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Script used for shutting down any virtual machines in the Preview environment
+# Script used for shutting down any virtual machines in the specified environment
+# When running this script make sure to pass the environment parameter - e.g. 'preview', 'preproduction'
 
 ####################################################
 # AWS first 
 
 aws_array=()
 
-# populate the array with preview ec2 instance ids 
+# populate the array with ec2 instance ids 
 while IFS= read -r line; do
     aws_array+=( "$line" )
-done < <( aws ec2 describe-instances --filters "Name=tag:Environment,Values=preview" --query 'Reservations[*].Instances[*].InstanceId' --output text )
+done < <( aws ec2 describe-instances --filters "Name=tag:Environment,Values=$1" --query 'Reservations[*].Instances[*].InstanceId' --output text )
 
 
 # loop through array and shutdown the instances 
@@ -19,18 +20,18 @@ do
 done
 
 
-########################################################
+######################################################
 # Azure equivalent 
 
 azure_array=()
 
-# populate the array with preview ec2 instance ids 
+# populate the array with the virtual machine names 
 while IFS= read -r line; do
     azure_array+=( "$line" )
-done < <( az resource list --resource-group preview_rg --resource-type Microsoft.Compute/virtualMachines --query "[].[name]" -o tsv )
+done < <( az resource list --resource-group $1_rg --resource-type Microsoft.Compute/virtualMachines --query "[].[name]" -o tsv )
 
-# loop through array and shutdown the instances 
+# loop through array and shutdown the vms 
 for i in "${azure_array[@]}"
 do
-    az vm stop --resource-group preview_rg --name $i
+    az vm stop --resource-group $1_rg --name $i
 done
--- support/preview_startup.sh
+++ support/preview_startup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# Script used for starting up and virtual machines in the Preview environment
+# Script used for starting up any virtual machines in the specified environment
+# When running this script make sure to pass the environment parameter - e.g. 'preview', 'preproduction'
 
-###########################################
+###############################################
 # AWS first 
 
 aws_array=()
 
-# populate the array with preview ec2 instance ids 
+# populate the array with ec2 instance ids 
 while IFS= read -r line; do
     aws_array+=( "$line" )
-done < <( aws ec2 describe-instances --filters "Name=tag:Environment,Values=preview" --query 'Reservations[*].Instances[*].InstanceId' --output text )
+done < <( aws ec2 describe-instances --filters "Name=tag:Environment,Values=$1" --query 'Reservations[*].Instances[*].InstanceId' --output text )
 
 
 # loop through array and start-up the instances 
@@ -18,19 +19,18 @@ do
     aws ec2 start-instances --instance-ids $i
 done
 
-
-############################################
+###############################################
 # Azure equivalent 
 
 azure_array=()
 
-# populate the array with preview ec2 instance ids 
+# populate the array with azure virtual machine names 
 while IFS= read -r line; do
     azure_array+=( "$line" )
-done < <( az resource list --resource-group preview_rg --resource-type Microsoft.Compute/virtualMachines --query "[].[name]" -o tsv )
+done < <( az resource list --resource-group $1_rg --resource-type Microsoft.Compute/virtualMachines --query "[].[name]" -o tsv )
 
-# loop through array and start-up the instances 
+# loop through array and start-up the vms 
 for i in "${azure_array[@]}"
 do
-    az vm start --resource-group preview_rg --name $i
+    az vm start --resource-group $1_rg --name $i
 done

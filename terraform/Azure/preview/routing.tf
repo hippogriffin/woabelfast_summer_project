66c196061729b1e1efbc5750fee7d4cf33a82d00
Marc Hughes <M.Hughes@kainos.com>
2018-10-12 10:30:08 +0100

    Removing uneeded route conflicting with virtual gateway and adding subnet to proxy servers

--- terraform/Azure/preview/routing.tf
+++ terraform/Azure/preview/routing.tf
@@ -2,10 +2,4 @@ resource "azurerm_route_table" "preview_route_table" {
   name                = "preview_route_table"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-
-route {
-    name = "azure_to_aws_jenkins_route"
-    address_prefix = "10.121.0.0/16"
-    next_hop_type = "VirtualNetworkGateway"
-  }
 }
